bdd9bd9e2796e15fcb002930622739839560ae32
Travis Avey <avey.travis@gmail.com>
2017-10-25 09:07:44 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Updating your Raspberry Pi"
+
 # install the raspberry pi updater
 sudo apt-get install rpi-update -y
 
@@ -12,3 +14,27 @@ sudo apt-get update
 # install the repositories
 sudo apt-get upgrade -y
 
+# install git to get the most up to date version of motion
+sudo apt-get install git
+
+# install a library depencency for motion
+sudo apt-get install libjpeg-dev
+
+# get the ip address
+ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+
+echo "Your IP address is $(ip)"
+echo "Installing motion"
+
+# clone the most up to date version of motion
+git clone http://github.com/sackmotion/motion 
+# change directory
+cd motion
+# gets software ready to build
+./configure
+# builds the source code with make
+make
+# install
+sudo make install 
+# move the config file to the user config location
+sudo mv /usr/local/etc/motion-dist.conf /usr/local/etc/motion.conf
